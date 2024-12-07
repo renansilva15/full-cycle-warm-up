8bd40ced5658c041f815d446990cc9f4d0276d05
renansilva15 <renangabrielsilva150@gmail.com>
2024-12-07 16:00:18 -0300

    chore: add prisma config

---
+++ 2-scalable-apis-with-nestjs/prisma/migrations/20241207185118_create_product_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" REAL NOT NULL
+);
